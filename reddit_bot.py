70ed9b18df21a6e6bc49aba6854a010794328fb0
asutaria28 <amsutaria28@gmail.com>
2020-11-05 17:29:48 -0500

    11/5/20 edits

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,15 +1,32 @@
 import praw
 
 
+"Requirement 1 - Calling an API and getting data from a website"
 reddit = praw.Reddit(client_id='VRDpHKf3yx2UDw',
                      client_secret='jXA-c7OqNOH3xPKu0crJzzaNxymldQ',
                      user_agent='<console:crime_bot:0.0.1 (u/bot_user2886)>',
                      username="bot_user2886"
                      )
 
-"Retrieving top 10 instances in the hot list on a subreddit"
-subreddit = reddit.subreddit("TrueCrime")
+"Requirement 2 - Populating a class"
 
-for post in subreddit.hot(limit=10):
-    print('-------------------')
-    print(post.title)
+
+class TrueCrime:
+
+    "Retrieving top 10 instances in the hot list on a subreddit"
+    subreddit = reddit.subreddit("TrueCrime")
+
+    for post in subreddit.hot(limit=10):
+        print('-------------------')
+        print(post.title)
+        print(post.url)
+
+
+class SerialKillers:
+
+    subreddit = reddit.subreddit("serialkillers")
+
+    for post in subreddit.hot(limit=10):
+        print('-------------------')
+        print(post.title)
+        print(post.url)
